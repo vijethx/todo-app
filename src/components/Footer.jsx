6048b7b60f0546d119dba3b97d7f54dcfcb6ef45
Vijeth <38877972+vijethx@users.noreply.github.com>
2025-03-17 00:03:34 +0530

    Footer?

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
-export function Footer(){
-    return(
-        <footer className="">Footer</footer>
-    )
+export function Footer() {
+    return (
+        <footer>
+            <p>
+                built by <a href="https://github.com/vijethx">vijethx</a> while
+                re-re-re-learning reactjs
+            </p>
+            <p>find source <a href="https://github.com/vijethx/todo-app">here</a></p>
+        </footer>
+    );
 }
