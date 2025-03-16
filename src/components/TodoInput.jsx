86a4f40b8c2af5b85d41d7f0f08d50909b1eeee3
Vijeth <38877972+vijethx@users.noreply.github.com>
2025-03-17 00:05:22 +0530

    TodoInput works, need to store now

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,5 +1,32 @@
-export function TodoInput(){
-    return(
-        <div className="">TodoInput</div>
-    )
+import { useState } from "react";
+
+export function TodoInput({ handleAddTodo }) {
+    const [inputValue, setInputValue] = useState("");
+    return (
+        <form
+            className="input-container"
+            onSubmit={(e) => {
+                e.preventDefault();
+                if (!inputValue) return; // to make sure there are no empty tasks
+                handleAddTodo(inputValue);
+                setInputValue("");
+            }}
+        >
+            <input
+                type="text"
+                placeholder="Add task"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button
+                type="submit"
+                // onClick={() => {
+                //     if (!inputValue) return; // to make sure there are no empty tasks
+                //     handleAddTodo(inputValue);
+                // }}
+            >
+                ➕
+            </button>
+        </form>
+    );
 }
