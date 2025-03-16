90c081212d5e9dfa5e6662208bbff43405df09d1
Vijeth <38877972+vijethx@users.noreply.github.com>
2025-03-17 01:16:11 +0530

    sticky footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
 export function Footer() {
     return (
         <footer>
-            <p>
-                built by <a href="https://github.com/vijethx">vijethx</a> while
-                re-re-re-learning reactjs
-            </p>
-            <p>find source <a href="https://github.com/vijethx/todo-app">here</a></p>
+            <div className="footer-text">
+                <p>
+                    built by <a href="https://github.com/vijethx">vijethx</a>{" "}
+                    while re-re-re-learning reactjs
+                </p>
+                <p>
+                    find source{" "}
+                    <a href="https://github.com/vijethx/todo-app">here</a>
+                </p>
+            </div>
         </footer>
     );
 }
