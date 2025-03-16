e292a0c358dfc95f6fa5b270c9689bd0501884da
Vijeth <38877972+vijethx@users.noreply.github.com>
2025-03-16 20:53:16 +0530

    Tasklist can be rendered by its completion state (hardcoded)

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,3 +1,15 @@
 export function TodoCard({ todo }) {
-  return <h3 className="">{todo.input}</h3>;
+    return (
+        <div className="card todo-item">
+            <p>{todo.input}</p>
+            <div className="todo-buttons">
+                <button disabled={todo.complete}>
+                    <h6>Done</h6>
+                </button>
+                <button>
+                    <h6>Delete</h6>
+                </button>
+            </div>
+        </div>
+    );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,18 @@
 import { TodoCard } from "./TodoCard";
 
 export function TodoList({ todos }) {
-  return (
-    <div className="">
-      {todos.map((todo, todoIndex) => (
-        <TodoCard key={todoIndex} todo={todo} />
-      ))}
-    </div>
-  );
+    const tab = "Open";
+    const filterTodosList =
+        tab === "All"
+            ? todos
+            : tab === "Completed"
+            ? todos.filter((i) => i.complete)
+            : todos.filter((i) => !i.complete);
+    return (
+        <>
+            {filterTodosList.map((todo, todoIndex) => (
+                <TodoCard key={todoIndex} todo={todo} />
+            ))}
+        </>
+    );
 }
